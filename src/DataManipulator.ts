37a3c86faf6186f69b95c9045d4177b8be9ad822
Pranav-Sandeep-Shinde <ss36507703@gmail.com>
2024-06-29 15:40:09 +0530

    Updated Column and column-pivot

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -17,12 +17,12 @@ export class DataManipulator {
     const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
     const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
     const ratio = priceABC / priceDEF;
-    const upperBound = 1 + (ratio * 0.01);
-    const lowerBound = 1 - (ratio * 0.01);
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
     return{
       price_abc: priceABC,
         price_def: priceDEF,
-          ratio:ratio,
+          ratio,
           timestamp : serverResponds[0].timestamp > serverResponds[1].timestamp ?
             serverResponds[0].timestamp : serverResponds[1].timestamp,
             upper_bound: upperBound,
